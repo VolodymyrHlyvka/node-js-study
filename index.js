87da99a921215287d6ebb51ffd609333dcbfa891
VolodymyrHlyvka <vhlyvka@gmail.com>
2024-06-10 20:19:58 +0300

    add ejs template engine

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
 
+// template engine setup
+app.set("view engine", "ejs");
+// tell node whre to find our templates (views folder)
+app.set("views", "views");
+
 const rootDir = require("./utils/path");
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const notFoundRoutes = require("./routes/404");
 
@@ -17,9 +22,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+// access to static files (css, fonts, images and ...)
 app.use(express.static(path.join(rootDir, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.router);
 app.use(shopRoutes);
 app.use(notFoundRoutes);
 
