f64e51a5ebace15e3789189849d01de1d3146161
VolodymyrHlyvka <vhlyvka@gmail.com>
2024-06-29 12:43:25 +0300

    add basic login/register flow

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,53 @@
 const Users = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 exports.login = (req, res) => {
-  const { name } = req.body;
-  Users.findOne({ name })
+  const { email, password } = req.body;
+  Users.findOne({ email })
     .then((user) => {
-      //   res
-      //     .cookie("loggedIn", "true", { maxAge: 900000, httpOnly: true })
       if (user) {
-        req.session.isLogged = true;
-        req.session.user = user;
-        res.send(user);
-      } else {
-        res.status(404).send("Not found");
+        return bcrypt
+          .compare(password, user.password)
+          .then((doMatch) => {
+            if (doMatch) {
+              req.session.isLogged = true;
+              req.session.user = user;
+              return req.session.save(() => {
+                res.send(user);
+              });
+            }
+            return res.send("Email or password are incorrect");
+          })
+          .catch((e) => {
+            console.log("bcrypt.compare error", e);
+          });
       }
+      res.status(404).send("Not found");
+    })
+    .catch((e) => {
+      console.log("error", e);
+    });
+};
+
+exports.register = (req, res) => {
+  const { email, password } = req.body;
+
+  Users.findOne({ email })
+    .then((user) => {
+      if (user) {
+        return res.send("User already exists");
+      }
+      return bcrypt.hash(password, 12).then((hassedPassword) => {
+        const newUser = new Users({ email, password: hassedPassword });
+        newUser
+          .save()
+          .then((user) => {
+            res.send(user);
+          })
+          .catch((e) => {
+            console.log("error", e);
+          });
+      });
     })
     .catch((e) => {
       console.log("error", e);
